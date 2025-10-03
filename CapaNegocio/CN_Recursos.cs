760b712a6ab25ed512d1f479b80b8b6765517dcf
DiazLianLautaro <invendol23@hotmail.com>
2025-10-03 17:50:45 -0300

    Primer backup

---
+++ CapaNegocio/CN_Recursos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Net.Mail;
+using System.IO;
+using System.Net;
+
+namespace CapaNegocio
+{
+    public class CN_Recursos
+    {
+        //retorna clave de 6 dígitos aleatoria
+        public static string GenerarClave()
+        {
+            string Clave = Guid.NewGuid().ToString("N").Substring(0,6);
+            return Clave;
+        }
+
+
+        //encriptación de TEXTO en SHA256
+        public static string ConvertSha256(string texto)
+        {
+            StringBuilder Sb = new StringBuilder();
+            //USAR LA REFERENCIA DE "System.Security.Cryptography"
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result) 
+                {
+                    Sb.Append(b.ToString("x2"));
+                }
+            }
+            return Sb.ToString();
+        }
+
+        public static bool EnviarCorreo(string correo, string asunto, string mensaje)
+        {
+            bool resultado = false;
+
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                using (var smtp = new SmtpClient())
+                {
+
+                    //programpruebas47@gmail.com
+                    mail.From = new MailAddress("norespoder47@demo.com");  //desde quien (correo ficticio sin dominio en gmail por ejemplo)
+                    mail.To.Add(correo);  //a quien
+                    mail.Subject = asunto;
+                    mail.Body = mensaje;
+                    mail.IsBodyHtml = true;
+
+
+                    smtp.UseDefaultCredentials = false; //IMPORTANTE debe ir antes de las credenciales 
+                    smtp.Host = "sandbox.smtp.mailtrap.io";
+                    smtp.Port = 2525;
+                    //smtp.Port = 465;
+                    smtp.EnableSsl = true; //Si o si para Mailtrap
+                    smtp.Credentials = new NetworkCredential("dc2253c6c3f535", "e8e5991b265b68");
+
+                    smtp.Send(mail);
+                }
+                ;
+                resultado = true;
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ CapaNegocio/CN_Usuarios.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+
+
+namespace CapaNegocio
+{
+    public class CN_Usuarios
+    {
+        private CD_Usuarios objCapaDato = new CD_Usuarios();
+
+        public List<Usuario> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            //Validaciones a campos nulos o vacios.
+            if(string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                Mensaje = "El Nombre del usuario no puede ser vacio";
+            }
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                Mensaje = "El Apellido del usuario no puede ser vacio";
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                Mensaje = "El Correo del usuario no puede ser vacio";
+            }
+
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                string clave = CN_Recursos.GenerarClave();
+                string asunto = "Creación de la Cuenta";
+                string mensajeCorreo = "<h3>Su cuenta fue creada correctamente</h3></br><p>Su contraseña para acceder es: !clave</p>";
+                mensajeCorreo = mensajeCorreo.Replace("!clave", clave);
+
+                bool respuesta = CN_Recursos.EnviarCorreo(obj.Correo, asunto, mensajeCorreo);
+
+                if (respuesta) 
+                {
+                    obj.Clave = CN_Recursos.ConvertSha256(clave);
+                    return objCapaDato.Registrar(obj, out Mensaje);
+                }
+                else
+                {
+                    Mensaje = "No se puede enviar el correo";
+                    return 0;
+                }
+
+
+            }
+            else 
+            {
+                return 0;
+            }
+
+
+
+        }
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            //Validaciones a campos nulos o vacios.
+            if (string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                Mensaje = "El Nombre del usuario no puede ser vacio";
+            }
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                Mensaje = "El Apellido del usuario no puede ser vacio";
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                Mensaje = "El Correo del usuario no puede ser vacio";
+            }
+
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
+
+    }
+}
